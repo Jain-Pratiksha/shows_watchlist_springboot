c5542b9aa3d7c78593bd7e22e38edec5d8a940ea
Jain-Pratiksha <@peps2650#>
2025-07-19 14:13:02 +0530

    logback-spring.xml added

--- src/main/java/com/shows_watchlist/ShowWatchlistApplication.java
+++ src/main/java/com/shows_watchlist/ShowWatchlistApplication.java
@@ -1,21 +1,9 @@
 package com.shows_watchlist;
 
-import io.swagger.v3.oas.annotations.*;
-import io.swagger.v3.oas.annotations.info.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition(
-		info = @Info(
-				title = "Show Watchlist APIs",
-				version = "1.0",
-				description = "API for managing a shows watchlist",
-				contact = @Contact(
-						name = "Show Watchlist Team : Pratiksha"
-				)
-		)
-)
 public class ShowWatchlistApplication {
 
 	public static void main(String[] args) {
